98f8fdb033fd435805bcce319fa6b58fe51664b4
Wong Kok Wah <wongkokwah@gmail.com>
2025-03-23 23:13:31 +0800

    upload picture to S3 bucket

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,10 +1,25 @@
 import createError from "http-errors";
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import { getAuctionById } from "./getAuction.js";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3.js";
 
-async function uploadAuctionPicture(event, context) {
+export async function uploadAuctionPicture(event, context) {
+
+  // {{AUCTIONS_HOST}}/auction/{{id}}/picture
+  const { id } = event.pathParameters;
+  // const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
+  
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
   try {
+    const uploadResult = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    console.log('uploadResult', uploadResult);
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "uploadAuctionPicture success!" }),
+      // image: result,
     };
   } catch (error) {
     console.log(error);
@@ -12,4 +27,4 @@ async function uploadAuctionPicture(event, context) {
   }
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
