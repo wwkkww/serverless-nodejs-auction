db051a741cc4733766616629e73d27469644195b
Wong Kok Wah <wongkokwah@gmail.com>
2025-03-24 09:12:17 +0800

    set picture url in dynamoDB

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -3,6 +3,7 @@ import middy from "@middy/core";
 import httpErrorHandler from "@middy/http-error-handler";
 import { getAuctionById } from "./getAuction.js";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3.js";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl.js";
 
 export async function uploadAuctionPicture(event, context) {
 
@@ -13,18 +14,21 @@ export async function uploadAuctionPicture(event, context) {
   
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
+
+  let updatedAuction;
   try {
-    const uploadResult = await uploadPictureToS3(auction.id + ".jpg", buffer);
-    console.log('uploadResult', uploadResult);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "uploadAuctionPicture success!" }),
-      // image: result,
-    };
+    const picUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    console.log('picUrl', picUrl);
+    updatedAuction = await setAuctionPictureUrl(auction.id, picUrl);
   } catch (error) {
     console.log(error);
     throw new createError.InternalServerError(error);
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
 }
 
 export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
--- src/lib/uploadPictureToS3.js
+++ src/lib/uploadPictureToS3.js
@@ -13,5 +13,14 @@ export async function uploadPictureToS3(key, pictureBuffer) {
     })
     .promise();
 
-  return result;
+  // result: {
+  //   Expiration: 'expiry-date="Tue, 25 Mar 2025 00:00:00 GMT", rule-id="ExpirePictures"',
+  //   ETag: '"1b12272bf2ff618e9f95df3cc98c3871"',
+  //   ServerSideEncryption: 'AES256',
+  //   Location: 'https://sls-auctions-bucket-wwkkww-dev.s3.ap-southeast-1.amazonaws.com/3d95dfd8-0348-4541-86db-635da94821c6.jpg',
+  //   key: '3d95dfd8-0348-4541-86db-635da94821c6.jpg',
+  //   Key: '3d95dfd8-0348-4541-86db-635da94821c6.jpg',
+  //   Bucket: 'sls-auctions-bucket-wwkkww-dev'
+  // }
+  return result.Location;
 }
